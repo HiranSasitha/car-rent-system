6b86461facbf323a6f302d0676776efa57e5418b
HiranSasitha <hiransasitha123@gamil.com>
2024-02-13 23:28:34 +0530

    first commit

---
+++ src/main/java/springboot/poscarrent/PosCarRentApplication.java
@@ -0,0 +1,13 @@
+package springboot.poscarrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosCarRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosCarRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot/poscarrent/PosCarRentApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.poscarrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosCarRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
